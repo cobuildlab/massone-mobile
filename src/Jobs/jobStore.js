6fb17c497245620b1b379eacd79c22d4a4d1803d
Jose Villalobos <joseavv4@gmail.com>
2018-11-29 13:50:50 -0400

    add job actions & store

---
+++ src/Jobs/jobStore.js
@@ -0,0 +1,22 @@
+import Flux from 'flux-state';
+import { storeErrorHandler } from '../utils';
+
+/**
+ * The Job store, to handle Jobs data events
+ * @extends Flux
+ */
+class JobStore extends Flux.DashStore {
+  constructor() {
+    super();
+
+    this.addEvent('GetJobs');
+
+    this.addEvent('GetJob');
+
+    this.addEvent('JobStoreError', storeErrorHandler);
+  }
+}
+
+const jobStore = new JobStore();
+
+export default jobStore;
