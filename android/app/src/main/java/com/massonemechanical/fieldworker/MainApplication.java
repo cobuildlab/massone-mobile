3b36fbfb0f2447f4be7e347d301ab2bf91ed7a02
Angel Lacret <alacret@gmail.com>
2019-05-12 12:14:31 -0400

    pod fixes

--- android/app/src/main/java/com/massonemechanical/fieldworker/MainApplication.java
+++ android/app/src/main/java/com/massonemechanical/fieldworker/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import com.rssignaturecapture.RSSignatureCapturePackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new RNFirebasePackage(),
             new RNFirebaseMessagingPackage(),
             new RNFirebaseNotificationsPackage(),
