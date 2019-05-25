799d06e665434620091f71ac9d8084aba0e3220c
Angel Lacret <alacret@gmail.com>
2019-05-25 09:46:29 -0400

    fixing android build

--- android/app/src/main/java/com/massonemechanical/fieldworker/MainApplication.java
+++ android/app/src/main/java/com/massonemechanical/fieldworker/MainApplication.java
@@ -3,15 +3,16 @@ package com.massonemechanical.fieldworker;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import io.invertase.firebase.RNFirebasePackage;
+import com.rumax.reactnative.pdfviewer.PDFViewPackage;
+import com.rssignaturecapture.RSSignatureCapturePackage;
+import com.imagepicker.ImagePickerPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
+
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
-import com.rssignaturecapture.RSSignatureCapturePackage;
-import com.rumax.reactnative.pdfviewer.PDFViewPackage;
-import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
-import com.imagepicker.ImagePickerPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,42 +23,41 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new PDFViewPackage(),
+                    new RSSignatureCapturePackage(),
+                    new ImagePickerPackage(),
+                    new RNGestureHandlerPackage(),
+                    new ReactNativeDocumentPicker(),
+                    new RNFirebasePackage(),
+                    new RNFirebaseMessagingPackage(),
+                    new RNFirebaseNotificationsPackage()
+            );
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+    };
+
     @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNFirebasePackage(),
-            new RNFirebasePackage(),
-            new RNFirebaseMessagingPackage(),
-            new RNFirebaseNotificationsPackage(),
-            new RSSignatureCapturePackage(),
-            new PDFViewPackage(),
-            new ReactNativeDocumentPicker(),
-            new ImagePickerPackage(),
-            new RNGestureHandlerPackage()
-      );
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
     }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
