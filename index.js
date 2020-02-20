130aba2dc89b7ef3a72faa2431175f81b260bec2
Moises Marquina <marquinaabreu@gmail.com>
2020-02-20 11:32:51 -0400

    test husky

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import { AppRegistry, Platform } from 'react-native';
 import App from './App';
-// import {name as appName} from './app.json';
+// import { name as appName } from './app.json';
 import { i18n } from './src/utils/i18n';
 import moment from 'moment';
 import 'moment-timezone';
