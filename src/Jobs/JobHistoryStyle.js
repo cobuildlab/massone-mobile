dfb51cbd1e5490ae325cea8ec30bbfedb893a0e7
Moises Marquina <marquinaabreu@gmail.com>
2020-02-22 12:52:17 -0400

    new design jobHistoryScreen

--- src/Jobs/JobHistoryStyle.js
+++ src/Jobs/JobHistoryStyle.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 import { BLUE_MAIN } from '../constants/colorPalette';
 
 export default StyleSheet.create({
@@ -6,22 +6,42 @@ export default StyleSheet.create({
     marginTop: 10,
   },
   listItem: {
-    marginLeft: 0,
-    paddingLeft: 18,
+    paddingHorizontal: 10,
+    paddingVertical: 4,
   },
   viewListItem: {
-    marginLeft: 5,
-    marginRight: 5,
+    // marginHorizontal: 5,
+  },
+  containerOwner: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textActionHistory: {
+    fontFamily: 'Roboto',
+    color: '#000000',
+    fontSize: Dimensions.get('window').width <= 360 ? 15 : 16,
   },
   textOwner: {
-    marginBottom: 5,
+    marginBottom: 2,
+    color: BLUE_MAIN,
+    fontFamily: 'Roboto',
+    fontWeight: '500',
+    fontSize: Dimensions.get('window').width <= 360 ? 17 : 18,
     textAlign: 'left',
     alignSelf: 'flex-start',
   },
+  iconEmployeAndLocation: {
+    color: BLUE_MAIN,
+    marginRight: 3,
+    fontSize: Dimensions.get('window').width <= 360 ? 18 : 20,
+  },
   textCreated: {
     marginTop: 5,
-    textAlign: 'right',
-    alignSelf: 'flex-end',
+    color: '#BBBBBB',
+    fontSize: Dimensions.get('window').width <= 360 ? 11 : 12,
+    fontFamily: 'Roboto',
+    // textAlign: 'right',
+    // alignSelf: 'flex-end',
   },
   rolesMainText: {
     marginBottom: 5,
@@ -29,6 +49,8 @@ export default StyleSheet.create({
     alignSelf: 'flex-start',
   },
   rolesText: {
-    color: BLUE_MAIN,
+    color: '#BBBBBB',
+    fontSize: Dimensions.get('window').width <= 360 ? 11 : 12,
+    fontFamily: 'Roboto',
   },
 });
